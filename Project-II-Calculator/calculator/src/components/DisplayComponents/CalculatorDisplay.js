022796e844ab244771d06ae8c6730cea9bc927e9
Paul Abernathy <paul.abernathy7@gmail.com>
2019-05-11 14:16:34 -0400

    components added

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return (
+    <div>
+      <button className={props.buttonStyle}>{props.text}</button>
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
